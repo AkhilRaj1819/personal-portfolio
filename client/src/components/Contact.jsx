0d36db2cc62848a3b3d05cf685e0a7f5c9c75796
AkhilRaj1819 <vysyaraju.akhilraju.s89@kalvium.community>
2025-07-05 22:00:02 +0530

    changed photo

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 import NavBar from './NavBar'
+import Hero2 from '../contact/Hero2'
 const Contact = () => {
+  const [isDark, setIsDark] = useState(false);
+      useEffect(() => {
+      document.body.style.backgroundColor = isDark ? '#000000' : '#F7F8FC';
+      document.body.style.color = isDark ? '#F7F8FC' : '#000000';
+    }, [isDark]);
   return (
     <div>
-      <NavBar/>
+      <NavBar isDark={isDark} setIsDark={setIsDark}/>
+      <Hero2 isDark={isDark}/>
     </div>
   )
 }
